2a3003e3b26c118714bf13ff75339228cbe7d6d1
Aman Malik <aman.malik@snapdeal.com>
2018-03-12 12:42:51 +0530

    webservice changes

--- WebService/app.py
+++ WebService/app.py
@@ -1,27 +1,56 @@
-import json
 import heapq
-import librosa
-import pandas as pd
-import numpy as np
-from flask import Flask
-from keras.models import load_model
-from predict_echo_feat import echonest_feature_maker
+import json
+import os
 
+import numpy as np
+import pandas as pd
+from flask import Flask
+from flask import jsonify
+from flask import request
+from keras.models import load_model
+
+from predict_echo_feat import echonest_feature_maker
 
 app = Flask(__name__)
 
+upload_folder = '/Users/namakilam/workspace/Datamonk/MusicSims/DataMonk--Music-Similarity/WebService/music_uploads'
 
-
+if not os.path.exists(upload_folder):
+    os.makedirs(upload_folder)
 
 ## for function getAllSongs
 # loading csv file (id-track)
-df = pd.read_csv("id_track.csv",low_memory = False)
+# df = pd.read_csv("id_track.csv",low_memory = False)
+# tracks = df[['track_id', 'title']]
+echonest_feature_columns = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness',
+                            'tempo', 'valence']
+columns = ['id', 'title']
+columns.extend(echonest_feature_columns)
 
 
+def load_tracks_data():
+    if not os.path.isfile(upload_folder + '/tracks_data.csv'):
+        return pd.DataFrame(columns=columns)
+    else:
+        return pd.read_csv(upload_folder + '/tracks_data.csv')[columns]
+
+
+track_data = load_tracks_data()
+
+last_track_id = track_data.size
+
+
+# for index, row in tracks.iterrows():
+#    track = dict()
+#    track['id']=row['track_id']
+#    track['title']=row['title']
+#    track_data.append(track)
 ## for function get_distance
 # reading the csv file (id-features)
-echo =pd.read_csv("echo_features.csv", low_memory = False)  
+# echo =pd.read_csv("echo_features.csv", low_memory = False)
 
+def get_track_id():
+    return int(track_data.shape[0]) + 1
 
 
 ## for function getFeatures
@@ -29,102 +58,80 @@ echo =pd.read_csv("echo_features.csv", low_memory = False)
 model = load_model('acc_model.h5')
 
 
+def check_allowed_file_format(filename):
+    return filename.endswith('.mp3')
+
+
 # helper function
-def compute_distance(dft, b):
+def compute_distance(echonest_features, track_id):
     # distance from 13K songs
     # uses min_heap concept
     h = []
-    
-    for i in range(dft.shape[0]):
-        curr = dft.iloc[i]
-        curr = curr.astype('float32')
+
+    for index, row in track_data.iterrows():
+        if row['id'] == track_id:
+            continue
         # distance calculatd here is always positive
-        d = np.linalg.norm(curr-b)    
+        curr_feat = row[echonest_feature_columns].values
+        d = np.linalg.norm(echonest_features - curr_feat)
+        info = dict()
+        info['distance'] = d
+        info['track_id'] = row['id']
+        info['title'] = row['title']
+        h.append(info)
 
-        if len(h) < 5:
-            heapq.heappush(h, -d)
-
-        else:
-            # pop largest value present in current heap
-            popped_value = heapq.heappushpop(h, -d)
-   
-    # make negative values positive again
-    h = np.abs(h)   
-    # sort and return list
-    h.sort()    
-    return h.tolist()
-
-
-
+    return heapq.nsmallest(5, h, key=lambda s: s['distance'])
 
 
 @app.route("/getAllSongs")
-def getAllSongs():    
-   
-    
-    idd = df.to_dict()['track_id']
-    title = df.to_dict()['title']
-    
-    # creating list of dictionaries
-    out = []
-    for i in range(df.shape[0]):
-        dict = {}
-        dict['Id'] = idd[i]
-        dict['Track'] = title[i]      
-        out.append(dict)  
-        
-    # creating json         
-    j = json.dumps(out)     
-    return(j)  
-
-
-
+def getAllSongs():
+    # idd = df.to_dict()['track_id']
+    # title = df.to_dict()['title']
+    data = dict()
+    data['data'] = track_data[['id', 'title']].values.tolist()
+    return jsonify(data)
+    # resp = Response(js, status=200, mimetype='application/json')
+    # return resp
 
 
 @app.route("/computeDistance")
-def get_distance(id, track): 
-    
+def get_distance():
     # row of features of the given id
-    
+    track_id = int(request.args.get('track_id'))
     ### NOTE - id requires to be of integer type!!!
-    voi = echo.loc[echo['track_id'] == id]
-    voi = voi.values
-    
-    # drop voi from the dataframe
-    temp_df = echo.drop(voi.index.tolist()[0])    
-  
+    echo_features = track_data[track_data['id'] == track_id]
+    echo_features = echo_features[echonest_feature_columns].values
     # gets list of minimum distances
-    min_dist = compute_distance(temp_df, voi)
-    
+    min_dist = compute_distance(echo_features, track_id)
+
     # make json object
-    d = json.dumps(min_dist)   
-    
+    data = dict()
+    data['title'] = track_data[track_data['id'] == track_id]['title'].values[0]
+    data['data'] = min_dist
+    d = json.dumps(data)
+
     return d
-    
-    
-    
-    
-    
-    
-@app. route("/getFeature")
-def upload_audio_features(audio):
-    
-## NEED TO CODE FOR THIS!!
-    # enter your folder path"
-#     base_path = ""
-# path = path + audio
 
-    
-    voi = echonest_feature_maker(path,name,model)
-    
-    min_dist = compute_distance(echo, voi)
-    
-    return min_dist
-    
 
-    
-    
-    
+@app.route("/uploadFileComputeDistance", methods=['POST'])
+def upload_audio_features():
+    if request.method == 'POST':
+        file = request.files['audio_file']
+        if file and check_allowed_file_format(file.filename):
+            save_path = os.path.join(upload_folder, file.filename)
+            file.save(save_path)
+            echonest_features = echonest_feature_maker(save_path, model)
+            track_id = get_track_id()
+            row = [track_id, file.filename]
+            row.extend(echonest_features.tolist())
+            track_data.loc[track_id] = row
+            track_data.to_csv(upload_folder + '/tracks_data.csv')
+            min_dist = compute_distance(echonest_features, track_id)
+            data = dict()
+            data['data'] = min_dist
+            d = json.dumps(data)
+            return d
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- WebService/predict_echo_feat.py
+++ WebService/predict_echo_feat.py
@@ -1,16 +1,14 @@
-
 # coding: utf-8
 
 # In[ ]:
 
 
-import os
 import warnings
-import numpy as np
-from scipy import stats
-import pandas as pd
+
 import librosa
-import keras
+import numpy as np
+import pandas as pd
+from scipy import stats
 
 
 def columns():
@@ -23,7 +21,7 @@ def columns():
     columns = []
     for name, size in feature_sizes.items():
         for moment in moments:
-            it = ((name, moment, '{:02d}'.format(i+1)) for i in range(size))
+            it = ((name, moment, '{:02d}'.format(i + 1)) for i in range(size))
             columns.extend(it)
 
     names = ('feature', 'statistics', 'number')
@@ -32,119 +30,105 @@ def columns():
     return columns.sort_values()
 
 
+def compute_features(filepath):
+    final = pd.DataFrame(index=[1], columns=columns(), dtype=np.float32)
 
-def compute_features(base_path):
-      
-    final = pd.DataFrame(index=[1],columns=columns(), dtype=np.float32)
-    
     def sub_features(name, values):
-            features[name, 'mean'] = np.mean(values, axis=1)
-            features[name, 'std'] = np.std(values, axis=1)
-            features[name, 'skew'] = stats.skew(values, axis=1)
-            features[name, 'kurtosis'] = stats.kurtosis(values, axis=1)
-            features[name, 'median'] = np.median(values, axis=1)
-            features[name, 'min'] = np.min(values, axis=1)
-            features[name, 'max'] = np.max(values, axis=1)
-            
+        features[name, 'mean'] = np.mean(values, axis=1)
+        features[name, 'std'] = np.std(values, axis=1)
+        features[name, 'skew'] = stats.skew(values, axis=1)
+        features[name, 'kurtosis'] = stats.kurtosis(values, axis=1)
+        features[name, 'median'] = np.median(values, axis=1)
+        features[name, 'min'] = np.min(values, axis=1)
+        features[name, 'max'] = np.max(values, axis=1)
+
     warnings.filterwarnings('error', module='librosa')
-            
-    try:    
-        for root, dirnames, filenames in os.walk(base_path):
-                row = 1
-                for filename in filenames: 
-                    print(filename)
-                    filepath=os.path.join(root, filename ).replace('\\', '/')
 
-                    features = pd.Series(index=columns(), dtype=np.float32)
+    try:
 
+        features = pd.Series(index=columns(), dtype=np.float32)
 
-                    x, sr = librosa.load(filepath, sr=None, mono=True)
+        x, sr = librosa.load(filepath, sr=None, mono=True)
+        #len_x = len(x)
+        #id_x = len_x/2
+        #low_x = mid_x - 15
+        #high_x = mid_x + 15
+        #x = x[low_x * sr : high_x * sr]
+        #             """ZERO CROSSING RATE
+        #                 The rate at which the signal changes from positive to negative or back.
+        #                 Key feature to classify percussive sounds.
+        #                 Percussion sounds: resonant, hyper-resonant, stony dull or dull.
+        #             """
+        zcr = librosa.feature.zero_crossing_rate(x, frame_length=2048, hop_length=512)
+        sub_features("zcr", zcr)
 
-    #             """ZERO CROSSING RATE 
-    #                 The rate at which the signal changes from positive to negative or back.
-    #                 Key feature to classify percussive sounds.
-    #                 Percussion sounds: resonant, hyper-resonant, stony dull or dull.
-    #             """
-                    zcr = librosa.feature.zero_crossing_rate(x, frame_length=2048, hop_length=512)
-                    sub_features("zcr", zcr)
+        #
+        #    Losely relates to the twelve different pitch classes.
 
-                # 
-                #    Losely relates to the twelve different pitch classes.
+        #    CHROMA CQT - Constant Q
+        #    It converts data series to frequency domain
 
-                #    CHROMA CQT - Constant Q
-                #    It converts data series to frequency domain
+        #    CHROMA CENS
 
-                #    CHROMA CENS
+        # """
+        cqt = np.abs(librosa.cqt(x, sr=sr, hop_length=512, bins_per_octave=12, n_bins=7 * 12, tuning=None))
+        assert cqt.shape[0] == 7 * 12
+        assert np.ceil(len(x) / 512) <= cqt.shape[1] <= np.ceil(len(x) / 512) + 1
 
-                # """
-                    cqt = np.abs(librosa.cqt(x, sr=sr, hop_length=512, bins_per_octave=12, n_bins=7*12, tuning=None))
-                    assert cqt.shape[0] == 7 * 12
-                    assert np.ceil(len(x)/512) <= cqt.shape[1] <= np.ceil(len(x)/512)+1
+        chroma_cqt = librosa.feature.chroma_cqt(C=cqt, n_chroma=12, n_octaves=7)
+        sub_features('chroma_cqt', chroma_cqt)
 
-                    chroma_cqt = librosa.feature.chroma_cqt(C=cqt, n_chroma=12, n_octaves=7)
-                    sub_features('chroma_cqt', chroma_cqt)
+        chroma_cens = librosa.feature.chroma_cens(C=cqt, n_chroma=12, n_octaves=7)
+        sub_features('chroma_cens', chroma_cens)
 
-                    chroma_cens = librosa.feature.chroma_cens(C=cqt, n_chroma=12, n_octaves=7)
-                    sub_features('chroma_cens', chroma_cens)
+        del cqt
 
-                    del cqt
+        # """TONNETZ
 
-                # """TONNETZ
+        # """
+        tonnetz = librosa.feature.tonnetz(chroma=chroma_cens)
+        sub_features('tonnetz', tonnetz)
 
-                # """
-                    tonnetz = librosa.feature.tonnetz(chroma=chroma_cens)
-                    sub_features('tonnetz', tonnetz)
+        # """ stft
+        # """
+        stft = np.abs(librosa.stft(x, n_fft=2048, hop_length=512))
+        assert stft.shape[0] == 1 + 2048 // 2
+        assert np.ceil(len(x) / 512) <= stft.shape[1] <= np.ceil(len(x) / 512) + 1
+        del x
 
-                # """ stft
-                # """
-                    stft = np.abs(librosa.stft(x, n_fft=2048, hop_length=512))
-                    assert stft.shape[0] == 1 + 2048 // 2
-                    assert np.ceil(len(x)/512) <= stft.shape[1] <= np.ceil(len(x)/512)+1
-                    del x
+        chroma_stft = librosa.feature.chroma_stft(S=stft ** 2, n_chroma=12)
+        sub_features('chroma_stft', chroma_stft)
 
+        rmse = librosa.feature.rmse(S=stft)
+        sub_features('rmse', rmse)
 
-                    chroma_stft = librosa.feature.chroma_stft(S=stft**2, n_chroma=12)
-                    sub_features('chroma_stft', chroma_stft)
+        f = librosa.feature.spectral_centroid(S=stft)
+        sub_features('spectral_centroid', f)
+        f = librosa.feature.spectral_bandwidth(S=stft)
+        sub_features('spectral_bandwidth', f)
+        f = librosa.feature.spectral_contrast(S=stft, n_bands=6)
+        sub_features('spectral_contrast', f)
+        f = librosa.feature.spectral_rolloff(S=stft)
+        sub_features('spectral_rolloff', f)
 
-                    rmse = librosa.feature.rmse(S=stft)
-                    sub_features('rmse', rmse)
+        mel = librosa.feature.melspectrogram(sr=sr, S=stft ** 2)
+        del stft
+        f = librosa.feature.mfcc(S=librosa.power_to_db(mel), n_mfcc=20)
+        sub_features('mfcc', f)
 
-                    f = librosa.feature.spectral_centroid(S=stft)
-                    sub_features('spectral_centroid', f)
-                    f = librosa.feature.spectral_bandwidth(S=stft)
-                    sub_features('spectral_bandwidth', f)
-                    f= librosa.feature.spectral_contrast(S=stft, n_bands=6)
-                    sub_features('spectral_contrast', f)
-                    f = librosa.feature.spectral_rolloff(S=stft)
-                    sub_features('spectral_rolloff', f)
+        return features.values
 
-                    mel = librosa.feature.melspectrogram(sr=sr, S=stft**2)
-                    del stft
-                    f = librosa.feature.mfcc(S=librosa.power_to_db(mel), n_mfcc=20)
-                    sub_features('mfcc', f)
-
-                    final.loc[row] = features
-    #                 print(final)
-    #                 print("ROW:::",row)
-                    row+=1
     except Exception as e:
         print('ERROR! Check path to the audio folder.')
-    
+
     return final
 
 
-def echonest_feature_maker(path,name,model):   
-    df = compute_features(path)
-    df.to_csv('input.csv')
-    
-    test_data = pd.read_csv("input.csv", low_memory=False)    
-    test_data.drop([0,1], inplace = True)
-    test_data.set_index("feature", inplace = True)
-    
-    test_data1 = np.expand_dims(test_data, axis=2)
-    
-    output = model.predict(test_data1)
-    
-    return output[0]
-    
+def echonest_feature_maker(path, model):
+    features = compute_features(path)
 
+    test_data1 = np.expand_dims(features, axis=2)
+
+    output = model.predict(np.array([test_data1]))
+
+    return output[0]
